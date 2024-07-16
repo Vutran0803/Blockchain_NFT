e9041713c3794342edc841b7e794845a6b81a1a2
shujiKen <.emailshujikenz@gmail.com>
2024-07-16 14:53:07 +0700

    fe

--- src/main/java/com/othree/dao/NFT.java
+++
@@ -1,8 +0,0 @@
-package com.othree.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface NFT extends JpaRepository<NFT, String>{
-
-}
--- src/main/java/com/othree/dao/Transaction.java
+++
@@ -1,7 +0,0 @@
-package com.othree.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Transaction extends JpaRepository<Transaction, String> {
-
-}
